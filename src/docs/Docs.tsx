34f7fe7e9f3d829813100e2c59499822e4e8fd7a
CJ Cenizal <cj@cenizal.com>
2023-06-21 16:48:33 -0700

    Extract library into src/lib and create space for docs.

---
+++ src/docs/Docs.tsx
@@ -0,0 +1,15 @@
+import { VuiSpacer, VuiTitle } from "../lib";
+
+export const Docs = () => {
+  return (
+    <div>
+      <VuiTitle size="l">
+        <h1>Vectara-UI docs</h1>
+      </VuiTitle>
+      <VuiSpacer size="m" />
+      <VuiTitle size="s">
+        <h2>Coming soon...</h2>
+      </VuiTitle>
+    </div>
+  );
+};
