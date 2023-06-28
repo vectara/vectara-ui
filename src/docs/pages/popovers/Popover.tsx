f16d80ad688a479ee3ac6d1a891fc137535fb4c6
CJ Cenizal <cj@cenizal.com>
2023-06-28 16:42:06 -0700

    Document VuiPopover.

---
+++ src/docs/pages/popovers/Popover.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { VuiButtonSecondary, VuiIcon, VuiOptionsList, VuiPopover } from "../../../lib";
+import { BiCaretDown } from "react-icons/bi";
+
+const options = [
+  { value: "apples", label: "Apples" },
+  { value: "windows", label: "Windows" },
+  { value: "penguins", label: "Penguins" }
+];
+
+export const Popover = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("apples");
+
+  return (
+    <VuiPopover
+      isOpen={isOpen}
+      setIsOpen={() => setIsOpen(!isOpen)}
+      button={
+        <VuiButtonSecondary
+          color="normal"
+          size="s"
+          icon={
+            <VuiIcon size="m">
+              <BiCaretDown />
+            </VuiIcon>
+          }
+        >
+          Tribe
+        </VuiButtonSecondary>
+      }
+    >
+      <VuiOptionsList
+        isSelectable
+        onSelectOption={(value) => {
+          setIsOpen(false);
+          setSelectedOption(value);
+          alert(`Selected ${value}`);
+        }}
+        selectedOption={selectedOption}
+        options={options}
+      />
+    </VuiPopover>
+  );
+};
