bd0c8b4ca5fb0f23eb2121ec1d20402223ebdb8e
CJ Cenizal <cj@cenizal.com>
2023-07-16 20:53:28 -0700

    Refine active page state.

--- src/lib/components/table/TablePagination.tsx
+++ src/lib/components/table/TablePagination.tsx
@@ -34,21 +34,26 @@ export const VuiTablePagination = ({ rowsPerPage, page, numPages, onSelectPage }
             </VuiButtonTertiary>
           </VuiFlexItem>
 
-          {items.map((item, index) => (
-            <VuiFlexItem grow={false} shrink={false} key={index}>
-              {item === "..." ? (
-                <div className="vuiTableManyPagesToken">{item}</div>
-              ) : (
-                <VuiButtonTertiary
-                  color={index === activeIndex ? "primary" : "neutral"}
-                  size="s"
-                  onClick={() => onSelectPage(item)}
-                >
-                  {item}
-                </VuiButtonTertiary>
-              )}
-            </VuiFlexItem>
-          ))}
+          {items.map((item, index) => {
+            const isActive = index === activeIndex;
+            return (
+              <VuiFlexItem grow={false} shrink={false} key={index}>
+                {item === "..." ? (
+                  <div className="vuiTableManyPagesToken">{item}</div>
+                ) : (
+                  <VuiButtonTertiary
+                    color={isActive ? "primary" : "neutral"}
+                    isInert={isActive}
+                    isSelected={isActive}
+                    size="s"
+                    onClick={() => onSelectPage(item)}
+                  >
+                    {item}
+                  </VuiButtonTertiary>
+                )}
+              </VuiFlexItem>
+            );
+          })}
 
           <VuiFlexItem grow={false} shrink={false}>
             <VuiButtonTertiary
