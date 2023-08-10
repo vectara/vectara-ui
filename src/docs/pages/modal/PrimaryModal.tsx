4c4889c4a6d73708639a26e6d798071ad9033930
CJ Cenizal <cj@cenizal.com>
2023-08-09 19:00:19 -0700

    Enable Popovers inside of Modals and Drawers.

--- src/docs/pages/modal/PrimaryModal.tsx
+++ src/docs/pages/modal/PrimaryModal.tsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
-import { VuiButtonPrimary, VuiButtonSecondary, VuiModal, VuiSpacer, VuiText, VuiTitle } from "../../../lib";
+import {
+  VuiButtonPrimary,
+  VuiButtonSecondary,
+  VuiModal,
+  VuiSearchSelect,
+  VuiSpacer,
+  VuiText,
+  VuiTitle
+} from "../../../lib";
+
+const options = [
+  { value: "a", label: "Caffeine-free" },
+  { value: "b", label: "Freeze dried" },
+  { value: "c", label: "Gluten-free" },
+  { value: "d", label: "Halal" },
+  { value: "e", label: "High fiber" },
+  { value: "f", label: "Kosher" },
+  { value: "g", label: "Lactose-free" },
+  { value: "h", label: "Low-carb" },
+  { value: "i", label: "No nuts" },
+  { value: "j", label: "Non-GMO" },
+  { value: "k", label: "Sugar-free" },
+  { value: "l", label: "Vegan" }
+];
 
 export const PrimaryModal = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
   return (
     <>
@@ -23,6 +47,21 @@ export const PrimaryModal = () => {
         <VuiText>
           <p>I just thought you should know that your modal is showing.</p>
         </VuiText>
+
+        <VuiSpacer size="m" />
+
+        <VuiSearchSelect
+          title="Select all that apply"
+          isOpen={isPopoverOpen}
+          setIsOpen={setIsPopoverOpen}
+          onSelect={() => undefined}
+          selected={[]}
+          options={options}
+        >
+          <VuiButtonSecondary color="neutral" size="s">
+            Meal preference
+          </VuiButtonSecondary>
+        </VuiSearchSelect>
 
         <VuiSpacer size="l" />
 
