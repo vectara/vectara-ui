5e665370b8508c16f40a22ab68cd5c9287ae2137
CJ Cenizal <cj@cenizal.com>
2023-08-24 21:07:05 -0700

    Add size prop to VuiIconButton.

--- src/docs/pages/button/IconButton.tsx
+++ src/docs/pages/button/IconButton.tsx
@@ -1,5 +1,13 @@
 import { BiStar } from "react-icons/bi";
-import { BUTTON_COLOR, VuiIconButton, VuiFlexContainer, VuiFlexItem, VuiIcon } from "../../../lib";
+import {
+  BUTTON_COLOR,
+  VuiIconButton,
+  VuiFlexContainer,
+  VuiFlexItem,
+  VuiIcon,
+  BUTTON_SIZE,
+  VuiSpacer
+} from "../../../lib";
 
 export const IconButton = () => {
   const icon = (
@@ -9,12 +17,24 @@ export const IconButton = () => {
   );
 
   return (
-    <VuiFlexContainer>
-      {BUTTON_COLOR.map((color) => (
-        <VuiFlexItem grow={false} key={color}>
-          <VuiIconButton icon={icon} color={color} />
-        </VuiFlexItem>
-      ))}
-    </VuiFlexContainer>
+    <>
+      <VuiFlexContainer>
+        {BUTTON_COLOR.map((color) => (
+          <VuiFlexItem grow={false} key={color}>
+            <VuiIconButton icon={icon} color={color} />
+          </VuiFlexItem>
+        ))}
+      </VuiFlexContainer>
+
+      <VuiSpacer size="s" />
+
+      <VuiFlexContainer>
+        {BUTTON_SIZE.map((size) => (
+          <VuiFlexItem grow={false} key={size}>
+            <VuiIconButton icon={icon} color="primary" size={size} />
+          </VuiFlexItem>
+        ))}
+      </VuiFlexContainer>
+    </>
   );
 };
