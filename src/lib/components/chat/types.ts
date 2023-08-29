d46c819907b2c87609ad9c17126482cff4a43dc8
Seif <seifaziz10@gmail.com>
2023-08-29 12:33:46 -0700

    Add language property to ChatTurn

--- src/lib/components/chat/types.ts
+++ src/lib/components/chat/types.ts
@@ -9,8 +9,30 @@ export type ChatTurn = {
   };
   answer?: string;
   query?: string;
+  language?: ChatLanguage;
   results?: SearchResult[];
 };
+
+export const CHAT_LANGUAGES = [
+  "auto",
+  "eng",
+  "deu",
+  "fra",
+  "zho",
+  "kor",
+  "ara",
+  "rus",
+  "tha",
+  "nld",
+  "ita",
+  "por",
+  "spa",
+  "jpn",
+  "pol",
+  "tur"
+] as const;
+
+export type ChatLanguage = (typeof CHAT_LANGUAGES)[number];
 
 export const CHAT_STYLE_ORDER = ["closed", "condensed", "tall", "fullScreen"] as const;
 export type ChatStyle = (typeof CHAT_STYLE_ORDER)[number];
