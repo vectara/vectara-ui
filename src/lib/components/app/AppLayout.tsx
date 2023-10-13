306462336e6e717dac392fd629dfecfd3e130897
CJ Cenizal <cj@cenizal.com>
2023-10-13 07:01:52 -0700

    Add full prop to VuiAppLayout.

--- src/lib/components/app/AppLayout.tsx
+++ src/lib/components/app/AppLayout.tsx
@@ -1,16 +1,22 @@
 import { ForwardedRef, forwardRef } from "react";
+import classNames from "classnames";
 import { VuiAppSideNav, Props as VuiAppSideNavProps } from "./appSideNav/AppSideNav";
 
 type Props = {
   children: React.ReactNode;
   navItems?: VuiAppSideNavProps["items"];
   navContent?: React.ReactNode;
+  full?: boolean;
 };
 
 export const VuiAppLayout = forwardRef(
-  ({ children, navItems, navContent }: Props, ref: ForwardedRef<HTMLDivElement>) => {
+  ({ children, navItems, navContent, full }: Props, ref: ForwardedRef<HTMLDivElement>) => {
+    const classes = classNames("vuiAppLayout", {
+      "vuiAppLayout--full": full
+    });
+
     return (
-      <div className="vuiAppLayout">
+      <div className={classes}>
         {(navItems || navContent) && (
           <div className="vuiAppLayout__sideNav">
             <VuiAppSideNav items={navItems} content={navContent} />
