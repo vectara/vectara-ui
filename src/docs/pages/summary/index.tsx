55bf2aa5dd31b0a544309b846193a73b006a23f5
CJ Cenizal <cj@cenizal.com>
2023-10-23 15:53:01 -0700

    Add markdown support to VuiSummary. (#127)

--- src/docs/pages/summary/index.tsx
+++ src/docs/pages/summary/index.tsx
@@ -1,14 +1,22 @@
 import { Summary } from "./Summary";
 import { Rtl } from "./Rtl";
+import { Markdown } from "./Markdown";
 
 const SummarySource = require("!!raw-loader!./Summary");
 const RtlSource = require("!!raw-loader!./Rtl");
+const MarkdownSource = require("!!raw-loader!./Markdown");
 
 export const summary = {
   name: "Summary",
   path: "/summary",
   examples: [
     {
+      name: "Markdown",
+      component: <Markdown />,
+      source: MarkdownSource.default.toString()
+    },
+    {
+      name: "Vanilla",
       component: <Summary />,
       source: SummarySource.default.toString()
     },
