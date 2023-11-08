03304699df90f76b70c8e7815b0cc1b5254c2bb4
CJ Cenizal <cj@cenizal.com>
2023-11-08 05:07:29 -0800

    Support rest prop in VuiSummaryCitation.

--- src/lib/components/summary/SummaryCitation.tsx
+++ src/lib/components/summary/SummaryCitation.tsx
@@ -6,13 +6,13 @@ type Props = {
   onClick?: () => void;
 };
 
-export const VuiSummaryCitation = ({ reference, isSelected, onClick }: Props) => {
+export const VuiSummaryCitation = ({ reference, isSelected, onClick, ...rest }: Props) => {
   const classes = classNames("vuiSummaryCitation", {
     "vuiSummaryCitation-isSelected": isSelected
   });
 
   return (
-    <button className={classes} onClick={onClick}>
+    <button className={classes} onClick={onClick} {...rest}>
       {reference}
     </button>
   );
