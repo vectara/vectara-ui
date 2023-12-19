f560a85d5a232ba349a34b73ee8888875aff3811
CJ Cenizal <cj@cenizal.com>
2023-12-19 14:32:57 -0800

    Add color prop to Horizontal Rule.

--- src/docs/pages/horizontalRule/HorizontalRule.tsx
+++ src/docs/pages/horizontalRule/HorizontalRule.tsx
@@ -1,3 +1,18 @@
-import { VuiHorizontalRule } from "../../../lib";
+import { BUTTON_COLOR, VuiHorizontalRule, VuiSpacer, VuiText } from "../../../lib";
 
-export const HorizontalRule = () => <VuiHorizontalRule />;
+export const HorizontalRule = () => {
+  return (
+    <>
+      {BUTTON_COLOR.map((color) => (
+        <>
+          <VuiText>
+            <p>{color}</p>
+          </VuiText>
+          <VuiSpacer size="m" />
+          <VuiHorizontalRule color={color} />
+          <VuiSpacer size="xl" />
+        </>
+      ))}
+    </>
+  );
+};
