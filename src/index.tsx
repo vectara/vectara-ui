9e60654711f925c6265ed66b91118bba470fc213
Deryk DeGuzman <deryk@vectara.com>
2024-05-06 20:22:08 +0000

    chore: migrate vui-source changes to 8128d65d6874fc1fb0be93ba6e2c9b8c6e1531a1

--- src/index.tsx
+++ src/index.tsx
@@ -16,3 +16,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+console.log("This is a small change to test the resulting automated PR. Will revert.");
