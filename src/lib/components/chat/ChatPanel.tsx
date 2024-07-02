1371d059c9278d80b89559981cdc21157cb44397
Deryk DeGuzman <deryk@vectara.com>
2024-07-02 03:13:45 +0000

    chore: migrate vui-source changes to v2.2.0

--- src/lib/components/chat/ChatPanel.tsx
+++ src/lib/components/chat/ChatPanel.tsx
@@ -24,6 +24,7 @@ export const VuiChatPanel = ({ title, onClose, children }: Props) => {
 
         <VuiFlexItem shrink={false} grow={false}>
           <VuiIconButton
+            aria-label="Hide chat"
             icon={
               <VuiIcon>
                 <BiX />
