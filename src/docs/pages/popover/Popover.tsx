2a7f6c36fbd4b72ac7474de41709451ba286a028
mrderyk <deryk@vectara.com>
2024-12-06 13:02:41 -0800

    chore: migrate vui-source changes to v1.0.2 (#169)

--- src/docs/pages/popover/Popover.tsx
+++ src/docs/pages/popover/Popover.tsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
-import { VuiButtonSecondary, VuiIcon, VuiOptionsList, VuiPopover } from "../../../lib";
+import {
+  AnchorSide,
+  VuiButtonSecondary,
+  VuiFormGroup,
+  VuiIcon,
+  VuiOptionsList,
+  VuiPopover,
+  VuiSelect,
+  VuiSpacer
+} from "../../../lib";
 import { BiCaretDown } from "react-icons/bi";
 
 const options = [
@@ -11,36 +20,54 @@ const options = [
 export const Popover = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState("apples");
+  const [anchorSide, setAnchorSide] = useState<AnchorSide>("right");
 
   return (
-    <VuiPopover
-      isOpen={isOpen}
-      setIsOpen={() => setIsOpen(!isOpen)}
-      header="Tribes"
-      button={
-        <VuiButtonSecondary
-          color="neutral"
-          size="s"
-          icon={
-            <VuiIcon size="m">
-              <BiCaretDown />
-            </VuiIcon>
-          }
-        >
-          Tribe: {selectedOption}
-        </VuiButtonSecondary>
-      }
-    >
-      <VuiOptionsList
-        isSelectable
-        isScrollable
-        onSelectOption={(value: string) => {
-          setIsOpen(false);
-          setSelectedOption(value);
-        }}
-        selected={selectedOption}
-        options={options}
-      />
-    </VuiPopover>
+    <>
+      <VuiFormGroup labelFor="anchorSideSelect" label="Anchor side">
+        <VuiSelect
+          id="anchorSideSelect"
+          options={[
+            { text: "Left", value: "left" },
+            { text: "Right", value: "right" }
+          ]}
+          value={anchorSide}
+          onChange={(event) => setAnchorSide(event.target.value as AnchorSide)}
+        />
+      </VuiFormGroup>
+
+      <VuiSpacer size="m" />
+
+      <VuiPopover
+        anchorSide={anchorSide}
+        isOpen={isOpen}
+        setIsOpen={() => setIsOpen(!isOpen)}
+        header="Tribes"
+        button={
+          <VuiButtonSecondary
+            color="neutral"
+            size="s"
+            icon={
+              <VuiIcon size="m">
+                <BiCaretDown />
+              </VuiIcon>
+            }
+          >
+            Tribe: {selectedOption}
+          </VuiButtonSecondary>
+        }
+      >
+        <VuiOptionsList
+          isSelectable
+          isScrollable
+          onSelectOption={(value: string) => {
+            setIsOpen(false);
+            setSelectedOption(value);
+          }}
+          selected={selectedOption}
+          options={options}
+        />
+      </VuiPopover>
+    </>
   );
 };
