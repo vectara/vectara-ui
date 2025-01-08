56f2bea9d96b37f1a242e25ff7aa839ca83f5241
Deryk DeGuzman <deryk@vectara.com>
2025-01-08 09:48:53 +0000

    chore: migrate vui-source changes to v3.10.0

--- src/docs/pages/button/Link.tsx
+++ src/docs/pages/button/Link.tsx
@@ -1,16 +1,30 @@
-import { VuiButtonPrimary } from "../../../lib";
+import { useState } from "react";
+import { VuiButtonPrimary, VuiSpacer, VuiToggle } from "../../../lib";
 
 export const Link = () => {
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
   return (
-    <VuiButtonPrimary
-      color="accent"
-      href="https://vectara.com"
-      target="_blank"
-      onClick={() => console.log("click")}
-      onMouseOver={() => console.log("mouse over")}
-      onMouseOut={() => console.log("mouse out")}
-    >
-      Visit Vectara.com
-    </VuiButtonPrimary>
+    <>
+      <VuiToggle
+        label="Disabled (not clickable, visual)"
+        checked={isDisabled}
+        onChange={() => setIsDisabled(!isDisabled)}
+      />
+
+      <VuiSpacer size="m" />
+
+      <VuiButtonPrimary
+        color="accent"
+        href="https://vectara.com"
+        target="_blank"
+        onClick={() => console.log("click")}
+        onMouseOver={() => console.log("mouse over")}
+        onMouseOut={() => console.log("mouse out")}
+        isDisabled={isDisabled}
+      >
+        Visit Vectara.com
+      </VuiButtonPrimary>
+    </>
   );
 };
