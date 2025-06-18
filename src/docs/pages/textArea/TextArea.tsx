5cdea193165c2d1f70cdf2dca061f608929eb9dd
Deryk DeGuzman <deryk@vectara.com>
2025-06-18 23:19:09 +0000

    chore: migrate vui-source changes to v5.4.0

--- src/docs/pages/textArea/TextArea.tsx
+++ src/docs/pages/textArea/TextArea.tsx
@@ -4,5 +4,13 @@ import { VuiTextArea } from "../../../lib";
 export const TextArea = () => {
   const [value, setValue] = useState<string | undefined>();
 
-  return <VuiTextArea id="textArea" value={value} onChange={(event) => setValue(event.target.value)} fullWidth />;
+  return (
+    <VuiTextArea
+      id="textArea"
+      onKeyDown={(e) => console.log("keydown", e)}
+      value={value}
+      onChange={(event) => setValue(event.target.value)}
+      fullWidth
+    />
+  );
 };
