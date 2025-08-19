7c56188f81714731dae202e47e736a41579a75c2
CJ Cenizal <cj@cenizal.com>
2025-08-19 05:02:01 -0700

    Add spacing prop to Timeline.

--- src/lib/components/timeline/Timeline.tsx
+++ src/lib/components/timeline/Timeline.tsx
@@ -3,13 +3,16 @@ import React from "react";
 
 type Props = {
   children: React.ReactNode;
+  spacing?: "none" | "s" | "m" | "l";
 };
 
-export const VuiTimeline = ({ children }: Props) => {
+export const VuiTimeline = ({ children, spacing = "m" }: Props) => {
   const childrenCount = React.Children.count(children);
   const wrappedChildren = React.Children.map(children, (child, index) => {
     const isLast = index === childrenCount - 1;
-    const classes = classNames("vuiTimelineContainer", { "vuiTimelineContainer--bordered": !isLast });
+    const classes = classNames("vuiTimelineContainer", `vuiTimelineContainer--spacing-${spacing}`, {
+      "vuiTimelineContainer--bordered": !isLast
+    });
     return <div className={classes}>{child}</div>;
   });
 
