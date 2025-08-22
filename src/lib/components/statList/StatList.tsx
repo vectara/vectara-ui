5e1a1cfd2106b54698362b1436c9a97436b68586
CJ Cenizal <cj@cenizal.com>
2025-08-22 10:54:05 -0700

    Add align config to StatList.

--- src/lib/components/statList/StatList.tsx
+++ src/lib/components/statList/StatList.tsx
@@ -5,12 +5,12 @@ import { VuiFlexItem } from "../flex/FlexItem";
 import { VuiLink } from "../link/Link";
 import { VuiSpacer } from "../spacer/Spacer";
 
-type Stat = { name: string; value: React.ReactNode };
+type Stat = { name: string; value: React.ReactNode; align: "start" | "center" | "end" | "baseline" | "stretch" };
 
-const VuiStat = ({ name, value }: Stat) => {
+const VuiStat = ({ name, value, align }: Stat) => {
   return (
     <>
-      <VuiFlexContainer alignItems="start" spacing="m">
+      <VuiFlexContainer alignItems={align ?? "start"} spacing="m">
         <VuiFlexItem grow={false} shrink={false} className="vuiStatName">
           <strong>{name}</strong>
         </VuiFlexItem>
@@ -32,7 +32,7 @@ export const VuiStatList = ({ stats, size = "s", className, ...rest }: Props) =>
 
   return (
     <div className={classes} {...rest}>
-      {stats.map(({ name, value }, index) => {
+      {stats.map(({ name, value, align }, index) => {
         const renderedValue =
           name === "url" || (typeof value === "string" && value.indexOf("http") === 0) ? (
             <VuiLink href={value as string} target="_blank">
@@ -43,7 +43,7 @@ export const VuiStatList = ({ stats, size = "s", className, ...rest }: Props) =>
           );
         return (
           <Fragment key={name}>
-            <VuiStat name={name} value={renderedValue} key={`${name}:${value}`} />
+            <VuiStat name={name} value={renderedValue} key={`${name}:${value}`} align={align} />
             {index < stats.length - 1 && <VuiSpacer size={size === "s" ? "s" : "xxs"} />}
           </Fragment>
         );
