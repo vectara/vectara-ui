e87233059f6de68e876ac5683c66749840a153ea
CJ Cenizal <cj@cenizal.com>
2025-09-11 14:46:37 -0700

    Refine text styles for Markdown content. Add support for blockquote, hr, table, sup, and pre and code blocks. (#267)

--- src/docs/pages/text/Text.tsx
+++ src/docs/pages/text/Text.tsx
@@ -18,9 +18,19 @@ export const Text = () => (
             <h6>Heading 6</h6>
             <p>
               Paragraph with some <strong>bold</strong> and <em>emphasized</em> words in it. Let's make this really,
-              really, really, super-duper, ridiculously long just so we can be sure the text wraps and you can get idea
-              of the line-height.
+              really, really, super-duper, <code>ridiculously long</code> just so we can be sure the text wraps and you
+              can get idea of the line-height.<sup>5</sup>
             </p>
+
+            <blockquote>
+              <p>
+                A long paragraph of text that's inside a blockquote. This is to demonstrate how blockquotes look when
+                used in conjunction with the VuiText component. The quick brown fox jumps over the lazy dog. The quick
+                brown fox jumps over the lazy dog.
+              </p>
+            </blockquote>
+
+            <hr />
 
             <p>
               Here's another paragraph with a <a href="#">link</a> in it.
@@ -41,8 +51,39 @@ export const Text = () => (
             </ol>
 
             <p>
-              <code>doc.title='Vectara'</code>
+              <code>(code) doc.title='Vectara'</code>
             </p>
+
+            <p>
+              <pre>(pre) doc.title='Vectara'</pre>
+            </p>
+
+            <table>
+              <thead>
+                <tr>
+                  <th> Line </th>
+                  <th> Entry </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> 1 </td>
+                  <td> A gull of memory crosses low, backlit and slow. </td>
+                </tr>
+                <tr>
+                  <td> 2 </td>
+                  <td> Quiet interest gathers on a breath I did not spend. </td>
+                </tr>
+                <tr>
+                  <td> 3 </td>
+                  <td> Moon counts the hours like coins within a tray. </td>
+                </tr>
+                <tr>
+                  <td> 4 </td>
+                  <td> I sign in trembling script: return by day. </td>
+                </tr>
+              </tbody>
+            </table>
           </VuiText>
 
           <VuiSpacer size="m" />
