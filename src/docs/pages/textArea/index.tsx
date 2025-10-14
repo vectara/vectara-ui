caa13fd67b3338610864496d3e5094e3a8e6ed3f
Aamir Khan <aamir.fastnu@gmail.com>
2025-10-14 11:56:17 +0500

    Allow textareas to be optionally resizable

--- src/docs/pages/textArea/index.tsx
+++ src/docs/pages/textArea/index.tsx
@@ -1,13 +1,22 @@
 import { TextArea } from "./TextArea";
+import { TextAreaResizeable } from "./TextAreaResizeable";
+
 const TextAreaSource = require("!!raw-loader!./TextArea");
+const TextAreaResizeableSource = require("!!raw-loader!./TextAreaResizeable");
 
 export const textArea = {
   name: "TextArea",
   path: "/textArea",
   examples: [
     {
+      name: "TextArea",
       component: <TextArea />,
       source: TextAreaSource.default.toString()
+    },
+    {
+      name: "Resizable TextArea",
+      component: <TextAreaResizeable />,
+      source: TextAreaResizeableSource.default.toString()
     }
   ]
 };
