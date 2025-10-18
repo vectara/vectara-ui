a4b6f2ccaea09808bafacf70b92b715a2e19e3d2
CJ Cenizal <cj@cenizal.com>
2025-10-17 17:18:11 -0700

    Add size prop to VuiLabel and VuiFormGroup.

--- src/docs/pages/label/Label.tsx
+++ src/docs/pages/label/Label.tsx
@@ -1,3 +1,18 @@
 import { VuiLabel } from "../../../lib";
+import { Subsection } from "../../components/Subsection";
 
-export const Label = () => <VuiLabel labelFor="inputId">Label</VuiLabel>;
+export const Label = () => {
+  return (
+    <>
+      <Subsection title="Small size (default)">
+        <VuiLabel labelFor="inputId">Label</VuiLabel>
+      </Subsection>
+
+      <Subsection title="Extra-small size">
+        <VuiLabel labelFor="inputId" size="xs">
+          Extra-small label
+        </VuiLabel>
+      </Subsection>
+    </>
+  );
+};
--- src/lib/components/form/label/Label.tsx
+++ src/lib/components/form/label/Label.tsx
@@ -4,10 +4,11 @@ type Props = {
   className?: string;
   labelFor?: string;
   children: React.ReactNode;
+  size?: "xs" | "s";
 };
 
-export const VuiLabel = ({ className, labelFor, children, ...rest }: Props) => {
-  const classes = classNames("vuiLabel", className);
+export const VuiLabel = ({ className, labelFor, children, size = "s", ...rest }: Props) => {
+  const classes = classNames("vuiLabel", `vuiLabel--${size}`, className);
 
   return (
     <label className={classes} htmlFor={labelFor} {...rest}>
