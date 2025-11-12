b81ed5c19bffe327664c07af6483a312e384641f
Aamir Khan <aamir.fastnu@gmail.com>
2025-11-12 17:07:50 +0500

    Add docs for templateAreas

--- src/docs/pages/grid/GridTemplateAreas.tsx
+++ src/docs/pages/grid/GridTemplateAreas.tsx
@@ -1,7 +1,35 @@
-import { VuiGrid, VuiGridItem, VuiPanel, VuiText } from "../../../lib";
+import { VuiGrid, VuiGridItem, VuiPanel, VuiSpacer, VuiText } from "../../../lib";
 
 export const GridTemplateAreas = () => (
   <>
+    <VuiText>
+      <p>Use `templateAreas` to set grid-template-areas.</p>
+      <p>grid-template-areas is a string-based grid area template.</p>
+      <ul>
+        <li>Each quoted string is a row in a 2D matrix.</li>
+        <li>Within a row, space-separated identifiers name the cells (e.g., header, sidebar).</li>
+        <li>Repeating the same identifier across adjacent cells merges them into one named grid area.</li>
+        <li>A single dot . denotes an unassigned cell.</li>
+        <li>All rows must have the same number of tokens.</li>
+        <li>
+          Elements join an area with grid-area: <code>grid-area: &lt;identifier&gt;.</code>
+        </li>
+      </ul>
+
+      <p>Example:</p>
+      <pre>
+        <code>
+          {`grid-template-areas: 
+\t"header header header"
+\t"sidebar content content"
+\t"sidebar content content"
+\t"footer footer footer";`}
+        </code>
+      </pre>
+    </VuiText>
+
+    <VuiSpacer size="m" />
+
     <VuiGrid
       templateAreas={`
         "header header header"
