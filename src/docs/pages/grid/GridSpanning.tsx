abaca173db37cee786a54dda42cef18d3d92a005
Aamir Khan <aamir.fastnu@gmail.com>
2025-11-14 18:06:42 +0500

    Address comment

--- src/docs/pages/grid/GridSpanning.tsx
+++ src/docs/pages/grid/GridSpanning.tsx
@@ -1,9 +1,8 @@
 import { VuiGrid, VuiGridItem, VuiPanel, VuiText } from "../../../lib";
 
 export const GridSpanning = () => (
-  <>
-    <VuiGrid templateColumns="repeat(4, 1fr)" spacing="m">
-      {/* 1st row */}
+  <VuiGrid templateColumns="repeat(4, 1fr)" spacing="m">
+    <>
       <VuiGridItem>
         <VuiPanel>
           <VuiText>
@@ -32,14 +31,17 @@ export const GridSpanning = () => (
           </VuiText>
         </VuiPanel>
       </VuiGridItem>
-      {/* 2nd row */}
+    </>
+
+    <>
       <VuiGridItem alignSelf="center" colSpan={2} rowSpan={1}>
         <VuiPanel>
           <VuiText>
-            <p>Spans 2 columns</p>
+            <p>Spans 2 columns and 1 row</p>
           </VuiText>
         </VuiPanel>
       </VuiGridItem>
+
       <VuiGridItem colSpan={2} rowSpan={5}>
         <VuiPanel fullHeight>
           <VuiText>
@@ -47,15 +49,24 @@ export const GridSpanning = () => (
           </VuiText>
         </VuiPanel>
       </VuiGridItem>
-      {/* 3rd row */}
-      <VuiGridItem colSpan={4} rowEnd={3}>
-        <VuiPanel>
+
+      <VuiGridItem colSpan={1} rowSpan={1}>
+        <VuiPanel fullHeight>
           <VuiText>
-            <p>Spans 4 columns and 1 row</p>
+            <p>Spans 1 column and 1 row</p>
           </VuiText>
         </VuiPanel>
       </VuiGridItem>
-      {/* 4th row */}
+      <VuiGridItem colSpan={2} rowSpan={2}>
+        <VuiPanel fullHeight>
+          <VuiText>
+            <p>Spans 2 columns and 1 row</p>
+          </VuiText>
+        </VuiPanel>
+      </VuiGridItem>
+    </>
+
+    <>
       <VuiGridItem colSpan={3}>
         <VuiPanel>
           <VuiText>
@@ -70,6 +81,6 @@ export const GridSpanning = () => (
           </VuiText>
         </VuiPanel>
       </VuiGridItem>
-    </VuiGrid>
-  </>
+    </>
+  </VuiGrid>
 );
