dae20534739d97e451436e27c14ce7f5a4508ead
CJ Cenizal <cj@cenizal.com>
2025-11-19 14:57:52 -0800

    Add align prop to VuiInfoTable. (#307)

--- src/docs/pages/infoTable/Simple.tsx
+++ src/docs/pages/infoTable/Simple.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { VuiInfoTable, VuiSpacer, VuiText, VuiTextColor, VuiToggle } from "../../../lib";
+import { VuiButtonPrimary, VuiInfoTable, VuiSelect, VuiSpacer, VuiText, VuiTextColor, VuiToggle } from "../../../lib";
+
+const alignOptions = [
+  { text: "Top", value: "top" },
+  { text: "Middle", value: "middle" },
+  { text: "Bottom", value: "bottom" }
+];
 
 const renderItem = ({ title, description }: { title: string; description: string }) => (
   <>
@@ -35,6 +41,7 @@ const renderConsumption = ({ amount, limit, unit }: { amount: number; limit: num
 
 export const Simple = () => {
   const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+  const [align, setAlign] = useState<"top" | "middle" | "bottom">("middle");
 
   const columns = [
     {
@@ -45,6 +52,11 @@ export const Simple = () => {
       name: "consumption",
       render: "Consumption",
       width: "200px"
+    },
+    {
+      name: "report",
+      render: "",
+      width: "100px"
     }
   ];
 
@@ -57,7 +69,14 @@ export const Simple = () => {
             description: "Aggregate size of all corpora after indexing, before any replication factor is applied."
           })
         },
-        consumption: { render: renderConsumption({ amount: 0, limit: 50, unit: "MiB" }) }
+        consumption: { render: renderConsumption({ amount: 0, limit: 50, unit: "MiB" }) },
+        report: {
+          render: (
+            <VuiButtonPrimary color="primary" size="s">
+              Report
+            </VuiButtonPrimary>
+          )
+        }
       }
     },
     {
@@ -68,7 +87,14 @@ export const Simple = () => {
             description: "Number of queries that can be issued per month for the account."
           })
         },
-        consumption: { render: renderConsumption({ amount: 0, limit: 15000, unit: "queries" }) }
+        consumption: { render: renderConsumption({ amount: 0, limit: 15000, unit: "queries" }) },
+        report: {
+          render: (
+            <VuiButtonPrimary color="primary" size="s">
+              Report
+            </VuiButtonPrimary>
+          )
+        }
       }
     },
     {
@@ -80,7 +106,14 @@ export const Simple = () => {
               "Number of requests for generative responses, like summarizations, that can be issued per month for the account."
           })
         },
-        consumption: { render: renderConsumption({ amount: 14999, limit: 15000, unit: "requests" }) }
+        consumption: { render: renderConsumption({ amount: 14999, limit: 15000, unit: "requests" }) },
+        report: {
+          render: (
+            <VuiButtonPrimary color="primary" size="s">
+              Report
+            </VuiButtonPrimary>
+          )
+        }
       }
     },
     {
@@ -91,16 +124,32 @@ export const Simple = () => {
             description: "Aggregate amount of ingested data."
           })
         },
-        consumption: { render: renderConsumption({ amount: 0, limit: 500, unit: "MiB" }) }
+        consumption: { render: renderConsumption({ amount: 0, limit: 500, unit: "MiB" }) },
+        report: {
+          render: (
+            <VuiButtonPrimary color="primary" size="s">
+              Report
+            </VuiButtonPrimary>
+          )
+        }
       }
     }
   ];
 
   return (
     <>
+      <VuiSelect
+        id="alignOptions"
+        options={alignOptions}
+        value={align}
+        onChange={(event) => setAlign(event.target.value as "top" | "middle" | "bottom")}
+      />
+
+      <VuiSpacer size="m" />
+
       <VuiToggle label="Has header" checked={isHeaderVisible} onChange={(e) => setIsHeaderVisible(e.target.checked)} />
       <VuiSpacer size="xl" />
-      <VuiInfoTable columns={columns} rows={rows} padding="s" isHeaderVisible={isHeaderVisible} />
+      <VuiInfoTable columns={columns} rows={rows} padding="s" isHeaderVisible={isHeaderVisible} align={align} />
     </>
   );
 };
