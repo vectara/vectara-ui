59e969edda01190e7b5f9f208c9f8414bdd6552b
Aamir Khan <aamir.fastnu@gmail.com>
2025-12-22 20:18:24 +0500

    Extract InfoListItem into its own component, scope sibling spacing (#329)

--- src/docs/pages/infoList/index.tsx
+++ src/docs/pages/infoList/index.tsx
@@ -1,11 +1,22 @@
 import { InfoList } from "./InfoList";
+import { HorizontalList } from "./HorizontalList";
+
 const InfoListSource = require("!!raw-loader!./InfoList");
+const HorizontalListSource = require("!!raw-loader!./HorizontalList");
 
 export const infoList = {
   name: "Info List",
   path: "/InfoList",
-  example: {
-    component: <InfoList />,
-    source: InfoListSource.default.toString()
-  }
+  examples: [
+    {
+      name: "Vertical",
+      component: <InfoList />,
+      source: InfoListSource.default.toString()
+    },
+    {
+      name: "Horizontal",
+      component: <HorizontalList />,
+      source: HorizontalListSource.default.toString()
+    }
+  ]
 };
