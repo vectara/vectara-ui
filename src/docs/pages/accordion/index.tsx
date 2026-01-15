39daf0d6377ed9198961002f46a67e56e8e0fb02
Aamir Khan <aamir.fastnu@gmail.com>
2026-01-15 21:55:08 +0500

    Add frameless, noPadding example

--- src/docs/pages/accordion/index.tsx
+++ src/docs/pages/accordion/index.tsx
@@ -1,11 +1,13 @@
 import { Accordion } from "./Accordion";
 import { AccordionFrameless } from "./AccordionFrameless";
 import { AccordionNoPadding } from "./AccordionNoPadding";
+import { AccordionFramelessNoPadding } from "./AccordionFramelessNoPadding";
 import { AccordionHeaderSize } from "./AccordionHeaderSize";
 
 const AccordionSource = require("!!raw-loader!./Accordion");
 const AccordionFramelessSource = require("!!raw-loader!./AccordionFrameless");
 const AccordionNoPaddingSource = require("!!raw-loader!./AccordionNoPadding");
+const AccordionFramelessNoPaddingSource = require("!!raw-loader!./AccordionFramelessNoPadding");
 const AccordionHeaderSizeSource = require("!!raw-loader!./AccordionHeaderSize");
 
 export const accordion = {
@@ -26,6 +28,11 @@ export const accordion = {
       name: "No padding",
       component: <AccordionNoPadding />,
       source: AccordionNoPaddingSource.default.toString()
+    },
+    {
+      name: "Frameless, no padding",
+      component: <AccordionFramelessNoPadding />,
+      source: AccordionFramelessNoPaddingSource.default.toString()
     },
     {
       name: "Header size",
