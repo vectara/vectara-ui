fef3ab7469a2ac1e20f863f7b3eb048db4593611
CJ Cenizal <cj@cenizal.com>
2026-03-11 19:25:38 -0700

    Add small size for VuiSelect. (#375)

--- src/docs/pages/select/Select.tsx
+++ src/docs/pages/select/Select.tsx
@@ -1,15 +1,45 @@
 import { useState } from "react";
-import { VuiSelect } from "../../../lib";
+import { VuiSelect, VuiSpacer } from "../../../lib";
 
 const options = [
+  { text: "Johann Baptist Christophorus Apollinaris von Albertini", value: "albertini" },
   { text: "David Brin", value: "brin" },
   { text: "Cormac McCarthy", value: "mccarthy" },
   { text: "Bret Easton Ellis", value: "ellis" }
 ];
 
 export const Select = () => {
-  const [value, setValue] = useState("ellis");
+  const [value, setValue] = useState("albertini");
+
   return (
-    <VuiSelect id="optionsList1" options={options} value={value} onChange={(event) => setValue(event.target.value)} />
+    <>
+      <VuiSelect
+        id="optionsList1"
+        size="s"
+        options={options}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+
+      <VuiSpacer size="m" />
+
+      <VuiSelect
+        id="optionsList2"
+        size="m"
+        options={options}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+
+      <VuiSpacer size="m" />
+
+      <VuiSelect
+        id="optionsList3"
+        size="l"
+        options={options}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+    </>
   );
 };
