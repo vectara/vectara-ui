c405f35354772ced5bcf6512771a917707f84f9d
CJ Cenizal <cj@cenizal.com>
2026-04-18 10:58:26 -0700

    Fix bug in which modal screen block was behind drawer.

--- src/lib/components/screenBlock/ScreenBlock.tsx
+++ src/lib/components/screenBlock/ScreenBlock.tsx
@@ -4,10 +4,12 @@ type Props = {
   onClick?: () => void;
   children: React.ReactNode;
   color?: "neutral" | "primary" | "danger" | "success";
+  // When a drawer is used to open a modal, ensure the modal's screen block is above the drawer.
+  type?: "default" | "modal";
 };
 
-export const VuiScreenBlock = ({ onClick, children, color = "neutral" }: Props) => {
-  const classes = classNames("vuiScreenBlock", `vuiScreenBlock--${color}`);
+export const VuiScreenBlock = ({ onClick, children, color = "neutral", type = "default" }: Props) => {
+  const classes = classNames("vuiScreenBlock", `vuiScreenBlock--${color}`, `vuiScreenBlock--${type}`);
   return (
     <div className={classes}>
       {children}
