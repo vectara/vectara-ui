4fa9edacc03acb871e0aa8b84fc1070e8d8ef715
CJ Cenizal <cj@cenizal.com>
2026-04-24 20:01:25 -0700

    Make tab title a ReactNode.

--- src/lib/components/tabs/types.ts
+++ src/lib/components/tabs/types.ts
@@ -2,7 +2,7 @@ export const TAB_SIZE = ["s", "m"] as const;
 export type TabSize = (typeof TAB_SIZE)[number];
 export type TabRoute = {
   href: string;
-  title: string;
+  title: React.ReactNode;
   render?: (tabLink: React.ReactNode) => React.ReactNode;
   testId?: string;
   isActive?: boolean;
