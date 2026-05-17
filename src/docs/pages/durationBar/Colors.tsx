0094d4a71b21896670678b064f60eb548cc0ef88
CJ Cenizal <cj@cenizal.com>
2026-05-17 02:48:17 -0700

    Support subdued and medium colors.

--- src/docs/pages/durationBar/Colors.tsx
+++ src/docs/pages/durationBar/Colors.tsx
@@ -1,10 +1,10 @@
 import { Fragment } from "react";
-import { PROGRESS_BAR_COLOR, VuiDurationBar, VuiSpacer } from "../../../lib";
+import { DURATION_BAR_COLOR, VuiDurationBar, VuiSpacer } from "../../../lib";
 
 export const Colors = () => {
   return (
     <>
-      {PROGRESS_BAR_COLOR.map((color) => (
+      {DURATION_BAR_COLOR.map((color) => (
         <Fragment key={color}>
           <VuiDurationBar window={100} start={10} end={70} color={color} />
           <VuiSpacer size="s" />
